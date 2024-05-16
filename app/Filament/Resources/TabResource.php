a11da1959157d65227406cdb7084ad6f6eb6ea7f
aotearoait <33107115+aotearoait@users.noreply.github.com>
2024-05-16 21:55:19 +1200

    Added key to component as advised, still same issue

--- app/Filament/Resources/TabResource.php
+++ app/Filament/Resources/TabResource.php
@@ -44,17 +44,17 @@ class TabResource extends Resource
                         Tab::make('Tab3')
                             ->schema([
 
-                               Livewire::make(CustomTabComponent1::class)
+                               Livewire::make(CustomTabComponent1::class)->key('custom-1')
                             ]),
                         Tab::make('Tab4')
                             ->schema([
 
-                                Livewire::make(CustomTabComponent2::class)
+                                Livewire::make(CustomTabComponent2::class)->key('custom-2')
                             ]),
                         Tab::make('Tab5')
                             ->schema([
 
-                                Livewire::make(CustomTabComponent3::class)
+                                Livewire::make(CustomTabComponent3::class)->key('custom-3')
                             ]),
                     ])
                     ->contained(false)
